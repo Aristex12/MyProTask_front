5a7d3ac602854c9c94e709a7083096dba65f4a09
Aris Maximilian Kuhs <aristex@hotmail.com>
2024-05-23 16:49:22 +0200

    Reapply "Merge branch 'wip_aris' of https://github.com/Aristex12/MyProTask_front into wip_aris"
    This reverts commit ab43c2201e9d87ee8e3a1457d06e6ba9c3493166.

--- MyProTask/src/app/app.module.ts
+++ MyProTask/src/app/app.module.ts
@@ -8,15 +8,14 @@ import { AsideComponent } from './modules/aside/aside.component';
 import { LoginComponent } from './modules/login/login.component';
 import { HomeComponent } from './modules/home/home.component';
 import { ProjectsComponent } from './modules/projects/projects.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { CalendarComponent } from './modules/calendar/calendar.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { EditprofileComponent } from './modules/editprofile/editprofile.component';
 import { UsersComponent } from './modules/users/users.component';
 import { TasksComponent } from './modules/tasks/tasks.component';
 import { HistoryComponent } from './modules/history/history.component';
-import { AuthService } from './servicios/auth/auth.service';
-import { JwtInterceptor } from './servicios/auth/jwt.interceptor';
+import { UserComponent } from './modules/user/user.component';
 
 
 
@@ -32,19 +31,18 @@ import { JwtInterceptor } from './servicios/auth/jwt.interceptor';
     EditprofileComponent,
     UsersComponent,
     TasksComponent,
-    HistoryComponent
+    HistoryComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,FullCalendarModule
+    HttpClientModule,
+    FullCalendarModule
   ],
-  providers: [
-    // AuthService,
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi:true}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
