ca31754eb9f108bed99aa7bb2e81dda230d8ee6d
KevinBayasMC <kevin.sebastian.external@eviden.com>
2024-05-24 11:45:12 +0200

    Revert "Version 3.0"
    This reverts commit 8e2a8ac449f53d713468eed9d09048f95bbcf9e9.

--- MyProTask/src/app/modules/login/login.component.ts
+++ MyProTask/src/app/modules/login/login.component.ts
@@ -1,58 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/servicios/auth/auth.service';
 import { LoginRequest } from 'src/app/servicios/auth/loginRequest';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
+export class LoginComponent {
+  loginForm=this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]], 
+    password:['']
+  })
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private authService: AuthService
-  ) {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-    });
-  }
+  constructor(private formBuilder:FormBuilder, private router:Router, private authService:AuthService) {}
 
   ngOnInit(): void {
-    // if(this.authService.isLoggedIn){
-    //   this.router.navigateByUrl('/home');
-    // } else {
-    //   this.authService.isLoggedIn = false;
-    // }
     this.authService.isLoggedIn = false;
   }
 
-  login() {
-    if (this.loginForm.valid) {
-      const loginRequest: LoginRequest = this.loginForm.value as LoginRequest;
-      this.authService.login(loginRequest).subscribe({
-        next: (response: any) => {
-          if (response && response.jwt) {
-            localStorage.setItem('loginToken', response.jwt);
-            this.router.navigateByUrl('/home');
-          } else {
-            console.log('Error de login');
+  login(){
+    if(this.loginForm.valid){
+      this.authService.login(this.loginForm.value as LoginRequest).subscribe({
+        next: (user:any) => {
+          if(user != null){
+            localStorage.setItem('idUser', user);
+            this.router.navigateByUrl("/home")
+          }else{
+            console.log("Error de login")
           }
         },
-        error: (error: any) => {
+        error: (error:any) =>{
           console.error(error);
         },
         complete: () => {
-          console.info('Inicio de sesión completado');
+          console.info("Inicio de sesión completado")
         }
       });
-      this.loginForm.reset();
-    } else {
+    }else{
       this.loginForm.markAllAsTouched();
     }
   }
