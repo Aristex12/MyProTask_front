998208a474404ee56ed9ce12e0675b6ce5678526
Anas Rami <anas.rami.external@eviden.com>
2024-05-30 16:37:12 +0200

    commit EditProfile

---
+++ myprotask/src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { AsideComponent } from './modules/aside/aside.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CalendarComponent } from './modules/calendar/calendar.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { EditprofileComponent } from './modules/editprofile/editprofile.component';
+import { UsersComponent } from './modules/users/users.component';
+import { TasksComponent } from './modules/tasks/tasks.component';
+import { HistoryComponent } from './modules/history/history.component';
+import { UserComponent } from './modules/user/user.component';
+import { NewUserComponent } from './modules/new-user/new-user.component';
+import { AllProjectsComponent } from './modules/projects-admin/all-projects.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { environment } from '../environments/environment';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    LoginComponent,
+    HomeComponent,
+    ProjectsComponent,
+    CalendarComponent,
+    EditprofileComponent,
+    UsersComponent,
+    TasksComponent,
+    HistoryComponent,
+    UserComponent,
+    NewUserComponent,
+    AllProjectsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FullCalendarModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ myprotask/src/app/models/user.ts
@@ -0,0 +1,25 @@
+export interface User {
+    idUser:number,
+    name:string,
+    lastName:string,
+    das:string,
+    email:string,
+    profilePic:string,
+    cv:string,
+    description:string,
+    role: {
+        idRole:number,
+        name:string
+    },
+    userCharacteristics: [
+        {
+            name:string,
+            idCaracteristic:number
+        },
+        {
+            name:string,
+            idCaracteristic:number
+        }
+    ],
+    active:boolean
+}
