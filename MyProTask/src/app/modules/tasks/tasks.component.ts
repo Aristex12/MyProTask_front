0f1e41c8e80dfc23ec9321b0f5291636d6832787
sergioramos-eviden <sergio.ramos.external@eviden.com>
2024-05-30 12:38:26 +0200

    Events Component

---
+++ MyProTask/src/app/modules/tasks/tasks.component.ts
@@ -0,0 +1,87 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TasksService } from 'src/app/servicios/task/tasks.service';
+import { Tasks } from 'src/app/models/tasks';
+
+
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  selectedTask: any;
+  tasksList: Tasks[] = [];
+  rol:string = "manager";
+  
+  selectTask(task: any) {
+    this.selectedTask = task;
+
+  }
+
+  constructor(private tasksService: TasksService) { }
+
+  ngOnInit(): void {
+    this.getData();
+    console.log(this.tasksList)
+
+  }
+
+
+
+  getColorByPriority(priority: string): string {
+    switch (priority) {
+      case 'HIGH':
+        return 'red';
+      case 'MID':
+        return 'blue';
+      case 'LOW':
+        return 'orange';
+      default:
+        return 'black';
+    }
+  }
+  getColorByPriorityBorder(priority: string): string {
+    switch (priority) {
+      case 'HIGH':
+        return '#F88379';
+      case 'MID':
+        return '#7BAFD4';
+      case 'LOW':
+        return '#FFDB58';
+      default:
+        return 'black';
+    }
+  }
+  //select priority
+  selectedPriority: string = '.';
+  filterTasksByPriority(priority: string) {
+    this.selectedPriority = priority;
+  }
+
+  getData() {
+    this.tasksService.getActiveTaskByUserId().subscribe({
+      next: (tasks:any) => {
+        this.tasksList = tasks;
+        this.selectTask(this.tasksList[0]);
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
+  }
+
+  //Insert TASK
+  InsertTask(task: any) {
+    //Nada por ahora
+  }
+  //EDIT TASK
+  updateTask(task: any) {
+    //Nada por ahora
+  }
+  
+
+}
